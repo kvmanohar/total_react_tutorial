c7469f65a11761bff15be4212722e492a6ad306d
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-22 15:41:35 +0000

    Simple Header component created

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Header from './header';
 
 ReactDOM.render(
 	<div>
-		<h1>New JSX Page</h1>
+		<Header title="Hello Props!" />
 		<p>This is a sample page React application.</p>
 	</div>,
 	document.querySelector('#root')
