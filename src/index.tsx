1c9dc13df0a304509a762dbb5e9e43bdbd2a9d7e
Manohar Kurapati <kvmanohar@gmail.com>
2019-12-22 15:08:56 +0000

    initial commit with base ts and webpack setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<div>
+		<h1>New JSX Page</h1>
+		<p>This is a sample page React application.</p>
+	</div>,
+	document.querySelector('#root')
+);
